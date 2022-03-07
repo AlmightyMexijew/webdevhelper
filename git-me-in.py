ce8990eb5dd54bc43b48664774cd91dc763ead0a
Gershon <CTepper625@gmail.com>
2022-03-07 23:19:15 +0200

    poop1

--- git-me-in.py
+++ git-me-in.py
@@ -1,5 +1,5 @@
 import os
 os.system("git add .")
 commitNote = str(input("What's our -m comment?"))
-os.system("git commit -m '%s'")
+os.system("git commit -m " + commitNote + "")
 os.system("git push")
